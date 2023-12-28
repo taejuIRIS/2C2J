a5327a5b9308c7d746263241b315499724139d2d
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2023-12-28 18:21:50 +0900

    :sparkles: Feat: responsedto 정의

---
+++ backend/src/main/java/com/todo/backend/controller/response/TodoResponsedto.java
@@ -0,0 +1,4 @@
+package com.todo.backend.controller.response;
+
+public class TodoResponsedto {
+}
---
+++ backend/src/main/java/com/todo/backend/controller/response/UserJoinResponsedto.java
@@ -0,0 +1,5 @@
+package com.todo.backend.controller.response;
+
+public class UserJoinResponsedto {
+    // 저장된 값 중 어떤 값을 받아올지 정의
+}
