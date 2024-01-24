c31da14f0634ebadbc0b5836bccce3b46f373b7e
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2024-01-25 00:08:17 +0900

    :zap: Feat: 최신 데이터 조회

--- backend/src/main/java/com/todo/backend/repository/RoutineRepository.java
+++ backend/src/main/java/com/todo/backend/repository/RoutineRepository.java
@@ -2,7 +2,13 @@ package com.todo.backend.repository;
 
 import com.todo.backend.entity.RoutineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface RoutineRepository extends JpaRepository<RoutineEntity, Integer> {
-    //List<RoutineEntity> findByStartdateBetweenAndEnddateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    // 가장 최신 데이터 시간 찾기
+    @Query("SELECT MAX(lastData) FROM RoutineEntity")
+    LocalDateTime findLastData();
 }
