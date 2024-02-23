ab300992ce0bd8abd433254ba9bc778e6b942529
JunH1004 <junejune040218@naver.com>
2024-02-23 19:57:52 +0900

    :lipstick: Feat 전역 스타일 코드 추가

---
+++ front/lib/my_style.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+var mainTheme = ThemeData(
+  scaffoldBackgroundColor: MyColors.black,
+  fontFamily: 'NotoSansKR',
+  textTheme: TextTheme(
+    bodyLarge: TextStyle(color: Colors.white),
+  ),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    )
+);
+
+class MyColors{
+  static final white = Colors.white;
+  static final black = Colors.black;
+  static final grey = Colors.grey;
+}
+class MyTextStyles{
+  static final h1 = TextStyle(color: Colors.black,fontSize: 32,fontWeight: FontWeight.bold);
+  static final h2 = TextStyle(color: Colors.black,fontSize: 24,fontWeight: FontWeight.bold);
+  static final h3 = TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold);
+  static final h4 = TextStyle(color: Colors.black,fontSize: 12,fontWeight: FontWeight.bold);
+
+  static final b1 = TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.normal);
+  static final b2 = TextStyle(color: Colors.black,fontSize: 12,fontWeight: FontWeight.normal);
+
+  static final s1 = TextStyle(color: MyColors.grey,fontSize: 12,fontWeight: FontWeight.normal);
+
+  static final b3 = TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.normal);
+  static final b4 = TextStyle(color: Colors.black,fontSize: 12,fontWeight: FontWeight.normal);
+}
