4e56956c3ca975576671b07039e9ada945ec036d
taejuIRIS <137082193+taejuIRIS@users.noreply.github.com>
2024-02-26 14:25:29 +0900

    :lipstick: Fix: 루틴 데이터 카드 수정

--- front/lib/widget/add_routine/add_routine_date_card.dart
+++ front/lib/widget/add_routine/add_routine_date_card.dart
@@ -3,40 +3,47 @@ import 'package:flutter/material.dart';
 import 'package:todotodo/my_style.dart';
 
 class AddRoutineDateCard extends StatefulWidget {
-  AddRoutineDateCard({super.key, required this.headText, required this.onChanged});
+  AddRoutineDateCard({Key? key, required this.headText, required this.onChanged})
+      : super(key: key);
   final String headText;
   Function(DateTime) onChanged;
+
   @override
   State<AddRoutineDateCard> createState() => _AddRoutineDateCardState();
 }
 
 class _AddRoutineDateCardState extends State<AddRoutineDateCard> {
-  String dateText = DateTime.now().toString();
+  late DateTime selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 48,
       decoration: BoxDecoration(
-        color: MyColors.white,borderRadius: BorderRadius.circular(16)
-      ),
+          color: MyColors.white, borderRadius: BorderRadius.circular(16)),
       child: Padding(
         padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('${widget.headText}',style: MyTextStyles.h3,),
+            Text('${widget.headText}', style: MyTextStyles.h3),
             Padding(
               padding: const EdgeInsets.all(4),
               child: GestureDetector(
-                onTap: (){
+                onTap: () {
                   showDatepicker(context);
                 },
                 child: Container(
                   width: 100,
                   decoration: BoxDecoration(
-                    color: MyColors.light_grey,borderRadius: BorderRadius.circular(12)
+                      color: MyColors.light_grey,
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Center(
+                    child: Text(
+                      '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                      style: MyTextStyles.b3,
+                    ),
                   ),
-                  child: Center(child: Text('${dateText}',style: MyTextStyles.b3,)),
                 ),
               ),
             )
@@ -46,29 +53,29 @@ class _AddRoutineDateCardState extends State<AddRoutineDateCard> {
     );
   }
 
-  void showDatepicker(BuildContext context){
+  void showDatepicker(BuildContext context) {
     showCupertinoDialog(
-                context: context, 
-                barrierDismissible: true,
-                builder: (BuildContext context){
-                  return Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      color: MyColors.white,
-                      height: 300,
-                      child: CupertinoDatePicker(
-                        mode: CupertinoDatePickerMode.date,
-                        showDayOfWeek: false,
-                        onDateTimeChanged: (value){
-                          setState(() {
-                            dateText = value.toString();
-                            widget.onChanged(value);
-                          });
-                        },
-                      ),
-                    ),
-                  );
-                }
-                );
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: MyColors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: selectedDate,
+              onDateTimeChanged: (value) {
+                setState(() {
+                  selectedDate = value;
+                  widget.onChanged(value);
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
   }
 }
